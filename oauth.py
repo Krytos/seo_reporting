334b7fb6775ecdf6860b955f544b45dd2f98dbb4
Krytos <kmeinon@gmail.com>
2023-06-15 12:07:50 +0200

    changed "from_client_secrets_file" to "from_client_config"

--- oauth.py
+++ oauth.py
@@ -1,4 +1,5 @@
 import os
+import json
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -9,7 +10,7 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 # If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
-SECRET = os.getenv('SECRET')
+SECRET = json.loads(os.getenv('SECRET'))
 
 
 def authenticate():
