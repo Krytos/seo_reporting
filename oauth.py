aae3a4e27c9e5b5c6c340051e471a6e60ae6badd
Krytos <kmeinon@gmail.com>
2023-06-15 21:52:35 +0200

    Changed flow from local server to fetch token

--- oauth.py
+++ oauth.py
@@ -27,7 +27,9 @@ SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 SECRET = json.loads(os.getenv('SECRET'))
 HOST = os.getenv('HOST')
 PORT = int(os.getenv('PORT'))
-SECRET['installed']['redirect_uris'] = [f'http://{HOST}/'] # [f'http://{HOST}:{PORT}/']
+# SECRET['installed']['redirect_uris'] = [f'http://{HOST}/']
+SECRET['installed']['redirect_uris'] = [f'http://{HOST}:{PORT}/']
+
 # st_oauth(config=SECRET)
 CLIENT_ID = SECRET['installed']['client_id']
 REDIRECT_URI = SECRET['installed']['redirect_uris'][0]
@@ -35,7 +37,8 @@ REDIRECT_URI = SECRET['installed']['redirect_uris'][0]
 SCOPE = SCOPES[0]
 SESSION['state'] = "state"
 STATE = SESSION['state']
-authorization_endpoint = f'https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPE}&state={STATE}&access_type=offline'
+# authorization_endpoint = f'https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPE}&state={STATE}&access_type=offline'
+authorization_endpoint = f'https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={CLIENT_ID}&redirect_uri=http://35.199.165.101:8501/&scope={SCOPE}&state={STATE}&access_type=offline'
 print(authorization_endpoint)
 def open_url():
 	open_script = f"""
