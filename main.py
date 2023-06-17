d77bbfa5d131d8a732d3239e1ae009543e6d3ff8
Krytos <kmeinon@gmail.com>
2023-06-17 17:30:12 +0200

    fixing datetime formating

--- main.py
+++ main.py
@@ -392,13 +392,15 @@ def main():
         )
         date = datetime.fromtimestamp(average_session_duration)
         date_compare = datetime.fromtimestamp(average_session_duration_compare)
-        date_delta = datetime(
-                f"+{date - date_compare}" if date >= date_compare else f"-{date_compare - date}"
-        ) if compare else None
+        date_delta = (
+            (date - date_compare if date >= date_compare else date_compare - date)
+            if compare
+            else None
+        )
         col1.metric(
             label="Durchschnittliche Sitzungsdauer",
             value=date.strftime('%M:%S'),
-            delta=date_delta.strftime('%M:%S') if compare else None,
+            delta=f"{'+' if date >= date_compare else '-'}{date_delta}" if compare else None,
         )
         col2.metric(
             label="Absprungrate",
